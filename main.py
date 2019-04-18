0824817bd5259eee12acb53a49cfe1dba2f74b0f
WMP\svogel <svogel@westmonroepartners.com>
2019-04-18 11:31:20 -0700

    Recommented and added another informative comment

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     # 2. The below code does not work because we are referencing variables that we have not made yet
     #    A variable is powerful because we can set the variable in one place and use it in another
     #    In order for the code to run, we need to have 2 adjectives and 1 noun.
-    adjective1 = input("Adjective: ")
-    adjective2 = input("Adjective: ")
-    noun1 = input("Noun: ")
+    # adjective1 = input("Adjective: ")
+    # adjective2 = input("Adjective: ")
+    # noun1 = input("Noun: ")
     # 3a. We now have a working madlib, but are missing 1 final noun to make it complete
 
     madlib = """
@@ -13,8 +13,9 @@ def main():
     place that is near a/an _""" + noun1 + """_ or up a/an _"""
     # 3b. We have created the second noun, but now we need to actually use it in the madlib
     #+ noun2 + """_."""
+    # The print function simply prints output into the console
     print(madlib)
 
 
 # 1. Above we defined the 'main' function of our program, but in order for it to run we need to call the main function
-main()
+# main()
