d09aa29a2d5db788a40e04c8abafd45b61602e33
Gage Black <gblack@westmonroepartners.com>
2019-04-19 11:09:14 -0700

    Added advanced_lesson and help files

---
+++ solutions/advanced_lesson.py
@@ -0,0 +1,34 @@
+# File will be used to demonstrate reading a file and string substitution
+# Input a file called madlib_example.txt
+# file contains a madlib, with words noted by underscores, i.e. _noun_
+
+def main():
+    file = open("madlib_example.txt", "r")
+    text = file.read()
+    print("\n-------------The File We are reading-------------")
+    print(text)
+    print("-------------------------------------------------\n")
+
+    # We will place our madlib, after word substitution into this variable
+    madlib = ""
+    # Loop through the provided text, by each word
+    for word in text.split(" "):
+        # find all of our marked words, think what identifies each madlib and how you can pick them out from regular words
+        # tip, python has many string funtions, utilize them here.
+        if word.startswith("_"):
+            # Next we need to get the word type from our input
+            # and prompt our user to input a word for madlib with the correct word type
+            wordType = word.strip("_")
+            word = input(wordType + ": ")
+            # manipulate the user input to be displayed correctly as needed
+            # we want to be able to identify our user inputed words easily in the final output
+            word = "_" + word + "_"
+        # after replacing the word we will need to concatenate the words back into a finished madlib
+        madlib += word + " "
+        
+    # Print out the final madlib
+    print("\n------------------MadLib------------------")
+    print(madlib)
+    print("--------------------------------------------\n")
+
+main()
